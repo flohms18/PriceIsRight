d954c2e68caf02792cd0b5a8f26ca7322308cc59
flotr18 <trouilletflorent@outlook.com>
2025-02-01 12:14:21 +0100

    Added while True Condition

--- PriceIsRight.py
+++ PriceIsRight.py
@@ -2,18 +2,20 @@ import random
 
 def PickRandomNumber():
     IntToGuess = random.randint(1,100)
-    IntUser = input("Pick a number between 1 & 100: ")
-    if IntToGuess < int(IntUser) :
-        print("Too High! Pick Again!")
-        IntUser = input("Pick a number between 1 & 100: ")
+    while True : 
+        try:
+            IntUser = int(input("Guess the number between 1 & 100 : "))
+            if IntUser < IntToGuess:
+                print("Too Low ! Pick Again!")
+            elif IntUser > IntToGuess:
+                print("Too High ! Pick Again!")
+            else :
+                print("Congrats ! You have found the right number:")
+                print(IntToGuess)
+                break
+        except ValueError:
+            print("Invalid Number")
 
-    elif IntToGuess > int(IntUser) :
-        print("Too Low! Pick Again!")
-        IntUser = input("Pick a number between 1 & 100: ")
-
-    else:
-        print("Good Guess! Congrats!")
-        return IntToGuess
 
 PickRandomNumber()
         
