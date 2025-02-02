98e9539acdcb8478ecea4a6dd3de452b04978a54
flotr18 <trouilletflorent@outlook.com>
2025-02-02 10:54:07 +0100

    Added attempts counter

--- PriceIsRight.py
+++ PriceIsRight.py
@@ -1,20 +1,31 @@
 import random
 
+
+
 def PriceisRight():
+    Attempt = 0
+    MaxAttempt = 3
     IntToGuess = random.randint(1,100)
-    while True : 
+    while Attempt < MaxAttempt : 
         try:
             IntUser = int(input("Guess the number between 1 & 100 : "))
+            Attempt += 1
             if IntUser < IntToGuess:
                 print("Too Low ! Pick Again!")
+                Attempt += 1
             elif IntUser > IntToGuess:
                 print("Too High ! Pick Again!")
             else :
                 print("Congrats ! You have found the right number:")
                 print(IntToGuess)
-                break
+                print(Attempt)
+                return
         except ValueError:
             print("Invalid Number")
+    print("Too many attemps!Game Over")
+    
+
+
 
 
 PriceisRight()
